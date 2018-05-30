c83158b2db68ceeb8cee5e342c2040c89cd40365
andreighervan <andrei.ghervan8@gmail.com>
2018-05-30 11:32:17 +0300

    Update files 30-05 v1 Signed-off-by: andreighervan <andrei.ghervan8@gmail.com>

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
-import Home from './components/Home';
-import UsersList from "./components/UsersList";
+import App from './App';
+import HomePage from './pages/HomePage';
+import UsersListPage from './pages/UsersListPage';
+import NotFoundPage from './pages/NotFoundPage';
+import AdminsListPage from './pages/AdminsListPage';
 
 export default [
     {
-        path:'/',
-        component:Home,
-        exact:true
-    },
-    {
-        path:'/users',
-        component:UsersList
+        ...App,
+        routes: [
+            {
+                ...HomePage,
+                path: '/',
+                exact: true
+            },
+            {
+                ...AdminsListPage,
+                path: '/admins'
+            },
+            {
+                ...UsersListPage,
+                path: '/users'
+            },
+            {
+                ...NotFoundPage
+            }
+        ]
     }
-]
+];
